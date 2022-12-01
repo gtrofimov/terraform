51e964b0811e467d99e9702dc038c8dc0a990057
gtrofimov <gtrofimov@parasoft.com>
2022-11-30 16:49:17 -0800

    Create main.tf

---
+++ lss/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+# Configiure the Docker provider
+provider "docker" {}
+
+# Create Container
+resource "docker_container" "lss" {
+  image = docker_image.lss.latest
+  name  = "lss"
+  ports {
+    internal = 8080
+    external = 8002
+  }
+  user = "root:root"
+
+# Mount LSS_DATA_DIR
+volumes {
+  container_path = "/usr/local/parasoft/license-server/data"
+  host_path = "/home/ec2-user/parasoft/terraform/lss/lss-data"
+  read_only = false
+  }
+
+# Mount docker.sock
+volumes {
+  container_path = "/var/run/docker.sock"
+  host_path = "/var/run/docker.sock"
+  read_only = false
+}
+
+# Mount parasoft-volume
+volumes {
+  volume_name = "parasoft-volume"
+  container_path = "/mnt/parasoft"
+  read_only = false
+}
+}
+
+
+# Deps
+# LSS Docker Image resource definition
+resource "docker_image" "lss" {
+  name         = "parasoft/lss:latest"
+  keep_locally = true
+}
+
+# LSS parasoft-volume
+resource "docker_volume" "parasoft_volume" {
+    name = "parasoft-volume"
+}
